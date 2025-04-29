224d3aea49b031abd2cb7778581ec69b6fc713e6
someone5678 <someone5678@users.noreply.github.com>
2025-04-29 15:01:36 +0530

    marble : Import QCOM audio effects from OnePlus 9R
    From:
    qssi-user 14 UKQ1.230924.001 1702916091049 release-keys
    
    Change-Id: I88bfb997f635ae86eaa0cde451e61816ebc2d0d7

--- marble-vendor.mk
+++ marble-vendor.mk
@@ -1485,6 +1485,9 @@ PRODUCT_PACKAGES += \
     sensors.ssc \
     sensors.touch.detect \
     libasphere \
+    libqcbassboost \
+    libqcreverb \
+    libqcvirt \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
     vendor.display.color@1.2 \
